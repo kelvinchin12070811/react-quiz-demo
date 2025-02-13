327f2042985a4a2c1fdbf11b78a3b034d5b582cb
Kelvin Chin <chinkaiwen_35@outlook.com>
2025-02-14 02:20:00 +0800

    Updates

---
+++ _next/static/chunks/app/about-us/page-978aed8c31b78622.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[851],{2317:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8395,23)),Promise.resolve().then(n.t.bind(n,1968,23))},8395:e=>{e.exports={heroContainer:"styles_heroContainer__ZkW9M"}}},e=>{var s=s=>e(e.s=s);e.O(0,[256,968,449,919,358],()=>s(2317)),_N_E=e.O()}]);
